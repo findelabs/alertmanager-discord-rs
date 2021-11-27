149b7cd2269e5bd8f829f405111849beeefb60ff
admin <circa.1991@gmail.com>
2021-11-27 11:15:34 -0500

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
 use axum::{
-	handler::Handler,
     extract::Extension,
-	http::StatusCode,
-	response::{IntoResponse},
+    handler::Handler,
+    http::StatusCode,
     http::{Request, Response},
+    response::IntoResponse,
     routing::{get, post},
-    AddExtensionLayer, Router,
-    Json
+    AddExtensionLayer, Json, Router,
 };
-use hyper_tls::HttpsConnector;
-use hyper::{client::HttpConnector, Body, Method};
-use hyper::header::{CONTENT_TYPE, HeaderValue};
-use std::{net::SocketAddr};
-use serde_json::{Value, json};
+use chrono::Local;
 use clap::{crate_version, App, Arg};
 use env_logger::{Builder, Target};
+use hyper::header::{HeaderValue, CONTENT_TYPE};
+use hyper::{client::HttpConnector, Body, Method};
+use hyper_tls::HttpsConnector;
 use log::LevelFilter;
+use serde_json::{json, Value};
 use std::io::Write;
-use chrono::Local;
+use std::net::SocketAddr;
 use std::sync::Arc;
 
 struct State {
-    webhook: String
+    webhook: String,
 }
 
 type HttpsClient = hyper::client::Client<HttpsConnector<HttpConnector>, Body>;
 
 #[tokio::main]
 async fn main() {
-
     let opts = App::new("alertmanager-discord-rust")
         .version(crate_version!())
         .author("Daniel F. <Verticaleap>")
@@ -41,7 +39,7 @@ async fn main() {
                 .required(false)
                 .env("LISTEN_PORT")
                 .default_value("8080")
-                .takes_value(true)
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("webhook")
@@ -50,7 +48,7 @@ async fn main() {
                 .required(true)
                 .value_name("DISCORD_WEBHOOK")
                 .help("Discord Webhook Endpoint")
-                .takes_value(true)
+                .takes_value(true),
         )
         .get_matches();
 
@@ -76,7 +74,9 @@ async fn main() {
         8080
     });
 
-    let shared_state = Arc::new(State { webhook: opts.value_of("webhook").unwrap().to_string() });
+    let shared_state = Arc::new(State {
+        webhook: opts.value_of("webhook").unwrap().to_string(),
+    });
 
     let https = HttpsConnector::new();
     let client = hyper::Client::builder().build::<_, hyper::Body>(https);
@@ -88,8 +88,8 @@ async fn main() {
         .layer(AddExtensionLayer::new(client))
         .layer(AddExtensionLayer::new(shared_state));
 
-	// add a fallback service for handling routes to unknown paths
-	let app = app.fallback(handler_404.into_service());
+    // add a fallback service for handling routes to unknown paths
+    let app = app.fallback(handler_404.into_service());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Listening on {}", addr);
@@ -100,12 +100,12 @@ async fn main() {
 }
 
 async fn health() -> Json<Value> {
-	log::info!("hit /health");
+    log::info!("hit /health");
     Json(json!({ "msg": "Healthy"}))
 }
 
 async fn echo(Json(payload): Json<Value>) -> Json<Value> {
-	log::info!("Returning /echo");
+    log::info!("Returning /echo");
     Json(payload)
 }
 
@@ -113,48 +113,110 @@ async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "nothing to see here")
 }
 
-async fn handler(Extension(client): Extension<HttpsClient>, Extension(state): Extension<Arc<State>>, Json(payload): Json<Value>) -> Response<Body> {
+async fn handler(
+    Extension(client): Extension<HttpsClient>,
+    Extension(state): Extension<Arc<State>>,
+    Json(payload): Json<Value>,
+) -> Response<Body> {
+    let body = generate_body(payload).await;
 
-	let body = generate_body(payload).await;
+    log::info!("\"log\": {}", &body);
 
-	log::info!("{}", &body);
-
-	let mut req = Request::builder()
+    let mut req = Request::builder()
         .method(Method::POST)
         .uri(&state.webhook)
-	    .body(Body::from(body.to_string()))
-	    .expect("request builder");
-    req.headers_mut().insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-
+        .body(Body::from(body.to_string()))
+        .expect("request builder");
+    req.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
     client.request(req).await.unwrap()
 }
 
 async fn generate_body(payload: Value) -> Value {
     let mut card = json!({
-        "username": "AlertManager",
         "content": "",
-        "embeds": [
-          {
-            "title": "Title",
-            "url": "https://google.com/",
-            "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
-            "color": 15258703,
-            "fields": [
-              {
-                "name": "Text",
-                "value": "More text",
-                "inline": true
-              }
-            ]
-          }
-        ]
+        "embeds": []
     });
 
-	if payload["commonAnnotations"]["description"].is_string() {
-		card["content"] = payload["commonAnnotations"]["description"].clone();
-	}
+    // Set content of main card
+    card["content"] = match payload["commonAnnotations"]["summary"].is_string() {
+        true => payload["commonAnnotations"]["summary"].clone(),
+        false => match payload["commonAnnotations"]["description"].is_string() {
+            true => payload["commonAnnotations"]["description"].clone(),
+            false => json!("Missing summary or description in common annotations"),
+        },
+    };
 
-	card
+    // Create embeds sub doc
+    let mut embeds = json!([{
+        "title": "",
+        "url": "",
+        "color": 0x95A5A6,
+        "fields": []
+    }]);
+
+    // Get count of alerts
+    let alert_count = match payload["alerts"].as_array() {
+        Some(alerts) => alerts.len(),
+        None => 0,
+    };
+
+    // Set card title
+    let title = format!(
+        "[{}:{}]: {}",
+        payload["status"].as_str().expect("status"),
+        alert_count,
+        payload["commonLabels"]["alertname"]
+            .as_str()
+            .expect("alertname")
+    );
+
+    // This needs to go into the embed title
+    embeds[0]["title"] = json!(title);
+
+    // Set prometheus url in embed doc
+    if payload["externalURL"].is_string() {
+        embeds[0]["url"] = payload["externalURL"].clone();
+    }
+
+    // Set embed doc color
+    embeds[0]["color"] = match payload["status"].as_str() {
+        Some("firing") => json!(0x992D22),
+        Some("resolved") => json!(0x2ECC71),
+        _ => json!(0x95A5A6),
+    };
+
+    // Gather all alerts to fields array for embed doc
+    let mut fields = Vec::new();
+    if let Some(alerts) = payload["alerts"].as_array() {
+        for alert in alerts {
+            log::info!("Processing alert");
+
+            let status = match alert["status"].is_string() {
+                true => alert["status"].as_str().expect("alert status"),
+                false => "unknown",
+            };
+
+            let alertname = match alert["labels"]["alertname"].is_string() {
+                true => alert["labels"]["alertname"]
+                    .as_str()
+                    .expect("alert label alertname"),
+                false => "missing alertname",
+            };
+
+            let name = format!("[{}]: {}", status, alertname);
+            let value = alert["annotations"]["description"].as_str();
+            let field = json!({"name": name, "value": value});
+            fields.push(field);
+        }
+    }
+
+    // Add fields to embeds
+    embeds[0]["fields"] = json!(fields);
+
+    // Build out main card
+    card["embeds"] = json!(embeds);
+
+    card
 }
-
